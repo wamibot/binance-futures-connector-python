376d01599c59094af0dced87b78bbbd0101ef066
oneforalone <oneforalone@proton.me>
2024-12-24 20:55:51 +0800

    add ed25519_signature to avoid conflict with binance-connector-python lib

--- binance/lib/authentication.py
+++ binance/lib/authentication.py
@@ -1,9 +1,10 @@
-import hmac
 import hashlib
+import hmac
 from base64 import b64encode
-from Crypto.PublicKey import RSA
+
 from Crypto.Hash import SHA256
-from Crypto.Signature import pkcs1_15
+from Crypto.PublicKey import ECC, RSA
+from Crypto.Signature import eddsa, pkcs1_15
 
 
 def hmac_hashing(secret, payload):
@@ -15,4 +16,11 @@ def rsa_signature(private_key, payload, private_key_pass=None):
     private_key = RSA.import_key(private_key, passphrase=private_key_pass)
     h = SHA256.new(payload.encode("utf-8"))
     signature = pkcs1_15.new(private_key).sign(h)
+    return b64encode(signature)
+
+
+def ed25519_signature(private_key, payload, private_key_pass=None):
+    private_key = ECC.import_key(private_key, passphrase=private_key_pass)
+    signer = eddsa.new(private_key, "rfc8032")
+    signature = signer.sign(payload.encode("utf-8"))
     return b64encode(signature)
